731a8a11b01bd9a3b7e3e3ac218b3530c25de0e9
Hunter Freyer <hjfreyer@google.com>
2015-05-14 19:03:50 -0400

    Hextime

---
+++ hextime/hextime.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+const (
+	hexSecInDay = 65536
+	hexSec      = time.Hour * 24 / hexSecInDay
+)
+
+func main() {
+	t := time.Now()
+	hour, min, sec := t.Clock()
+	nsec := t.Nanosecond()
+
+	timeIntoDay := (time.Duration(hour)*time.Hour +
+		time.Duration(min)*time.Minute +
+		time.Duration(sec)*time.Second +
+		time.Duration(nsec)*time.Nanosecond)
+
+	hexTime := int64(timeIntoDay / hexSec)
+	fmt.Println(strings.ToUpper(fmt.Sprintf("%x", hexTime)))
+
+}
