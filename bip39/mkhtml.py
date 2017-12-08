bc6458af9dd488ce4084299725366fb2e2436f56
Hunter Freyer <hjfreyer@turing.nyc.corp.google.com>
2017-12-08 12:52:57 -0500

    Change to octal, style updates

--- bip39/mkhtml.py
+++ bip39/mkhtml.py
@@ -4,25 +4,21 @@ import math
 
 print '''
 <style>
-h1 {
-  text-align: center;
+.page {
   page-break-before: always;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   font-family: monospace;
 }
 
 .pagenum {
-  text-align: center;
   font-family: monospace;
 }
 
-.colset {
-  display: flex;
-  font-size: 11px;
-  justify-content: space-between;
-}
-
 pre {
-  width: 115px;
+  marign: auto;
+  font-size: 11px;
 }
 
 </style>
@@ -32,21 +28,36 @@ with open('wordlist.txt') as f:
   words = [word.strip() for word in f]
 
 ROWS = 64
-COLS = 6
+COLS = 5
 PAGES = int(math.ceil((0.0+len(words))/(ROWS*COLS)))
+
+maxWordLen = max(len(x) for x in words)
+
+def pageIdxToWordIdx(page, row, col):
+  idx = page * ROWS * COLS
+  idx += col * ROWS
+  idx += row
+  return idx
+
 for pageIdx in range(PAGES):
-  print '''<h1>BIP 39 Wordlist</h1>
-  <div class="colset">'''
+  pageStartIdx = pageIdx*ROWS*COLS
+  print '''<div class="page"><h1>BIP 39 Wordlist</h1><pre>'''
 
   for colIdx in range(COLS):
-    print '''<pre>'''
-    for rowIdx in range(ROWS):
-      idx = pageIdx*ROWS*COLS + colIdx*ROWS + rowIdx
+    if pageIdxToWordIdx(pageIdx, 0, colIdx) < len(words):
+      print 'DECI OCTA WORD' + (' ' * (maxWordLen - 4)) + ' ',
+  print
+  for colIdx in range(COLS):
+    if pageIdxToWordIdx(pageIdx, 0, colIdx) < len(words):
+      print '==== ==== ' + ('=' * maxWordLen) + ' ',
+  print
+  for rowIdx in range(ROWS):
+    for colIdx in range(COLS):
+      idx = pageIdxToWordIdx(pageIdx, rowIdx, colIdx)
       if idx < len(words):
-        print '%04d %03X %s' % (idx, idx, words[idx])
-    print '''</pre>'''
-
-
-
-  print '''</div>
-  <div class="pagenum">%d/%d</div>''' % (pageIdx + 1, PAGES)
+        word = words[idx]
+        word += ' ' * (maxWordLen - len(word))
+        print '%04d %04o %s ' % (idx, idx, word),
+    print
+  print '''</pre>'''
+  print '''<div class="pagenum">%d/%d</div></div>''' % (pageIdx + 1, PAGES)
